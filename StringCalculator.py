2482107d144096c0d70760e018982beedd9bd42c
Yukta1706 <75121611+Yukta1706@users.noreply.github.com>
2024-07-16 10:48:34 +0530

    Test_case_3

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,7 +1,17 @@
-#test case 1 & 2
+#test case 1 & 2 & 3
 def add (y): 
   if y == " " or y =="0" :
     return 0
-  else :
-    return False
+  elif ("y"):
+    x= y.split(',')
+    num1 = int(x[0].strip())
+    num2 = int(x[1].strip())
+   
+    # addition
+    c = num1 + num2
+    
+    # store result into the string
+    result_str = str(c)
+    
+    return result_str
     
