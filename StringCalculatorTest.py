496e9d33a6fe505049c45c166d9805bd0fc30a03
Yukta1706 <75121611+Yukta1706@users.noreply.github.com>
2024-07-16 11:10:42 +0530

    Comment_3_1

--- StringCalculatorTest.py
+++ StringCalculatorTest.py
@@ -9,7 +9,7 @@ class TestStringCalculator(unittest.TestCase):
                 self.assertEqual(add("0"), 0)
                 
        def test_expectSumForTwoNumberst(self):
-               self.assertEqual(add("1,2"), 3)
+                self.assertEqual(add("1,2"), 3)
                 
         #def test_ignoreNumbersGreaterThan1000(self):
                # self.assertEqual(add("1,1001"), 1)
