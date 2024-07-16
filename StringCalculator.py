3b3315656366522a9a065cec8ae64d30af49750c
Yukta1706 <75121611+Yukta1706@users.noreply.github.com>
2024-07-16 11:22:53 +0530

    test_case_3_8

--- StringCalculator.py
+++ StringCalculator.py
@@ -2,14 +2,20 @@
 def add(y):
     if y == " " or y == "0":
         return 0
-    elif("y"):
-            x = y.split(',')
+    elif "," in y:
+        x = y.split(',')
+        if len(x) != 2:
+            return "Invalid input: Please provide two numbers separated by a comma."
+        
+        try:
             num1 = int(x[0].strip())
             num2 = int(x[1].strip())
-            c = num1 + num2
-            return c
+            result = num1 + num2
+            return result
+        except ValueError:
+            return "Invalid input: Please provide valid integers separated by a comma."
     else:
-        return
+        return "Invalid input: Please provide two numbers separated by a comma."
    
 
     
